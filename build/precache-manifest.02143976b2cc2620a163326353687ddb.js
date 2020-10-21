02aa17309f4d1a17ef748e9cc0a5e5f4618b033d
Bryson2121 <70928413+Bryson2121@users.noreply.github.com>
2020-10-20 22:32:10 -0500

    Add files via upload

---
+++ build/precache-manifest.02143976b2cc2620a163326353687ddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd0dd2186d386619f3334f332f57b4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "568e73647fdff5326c53",
+    "url": "/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "2ed3536959f4df0d1bc0",
+    "url": "/static/css/main.40961547.chunk.css"
+  },
+  {
+    "revision": "568e73647fdff5326c53",
+    "url": "/static/js/2.afda65fa.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.afda65fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed3536959f4df0d1bc0",
+    "url": "/static/js/main.1af05b29.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
